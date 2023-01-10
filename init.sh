bb6cdfd3aeded16db71c7fb7050806cd044bb136
Daniel Arndt <dan@arndt.ca>
2023-01-10 08:49:10 -0400

    Fix init script

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
-ln -s ~/.vim/vimrcs/vimrc.vim ~/config/nvim/init.vim
+mkdir -p ~/.config/nvim
+ln -s ~/.vim/vimrcs/vimrc.vim ~/.config/nvim/init.vim
 git submodule update --init --recursive
-syspip3 install flake8
+PIP_USER='yes' PIP_REQUIRE_VIRTUALENV="" /usr/bin/python3 -m pip install flake8
