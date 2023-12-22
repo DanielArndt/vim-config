f43060a7269e8c502eacb2b8aa426f655a5d8781
Daniel Arndt <dan@arndt.ca>
2023-12-22 11:51:35 -0400

    Remove pip installed flake8

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
 mkdir -p ~/.config/nvim
 ln -s ~/.vim/vimrcs/vimrc.vim ~/.config/nvim/init.vim
 git submodule update --init --recursive
-PIP_USER='yes' PIP_REQUIRE_VIRTUALENV="" /usr/bin/python3 -m pip install flake8
